b4494d4e28c165fe945851196ff23abf429139c7
cesar galindo <csgalindos@hotmail.com>
2019-10-06 23:29:14 -0500

    iniciando en react

---
+++ src/component/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/style/App.scs';
+
+const Primer = () => {
+  <h1>revision</h1>
+};
+
+export default Primer;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Primer from './component/App';
+
+
+ReactDOM.render(< Primer />,document.getElementById('App'));
+
