9e8f88f9d36f8cd295f0ab0af7cdeb9af8f97157
cesar galindo <csgalindos@hotmail.com>
2019-10-30 20:23:44 -0500

    credit card

--- src/component/CreditCard.jsx
+++ src/component/CreditCard.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import '../assets/style/components/CreditCard.scss';
 
 const CreditCard = () => {
-  render () {
   return (
     <div className='body'>
       <div className='credit-card'>
         <div className='credit-card__logo'>Logo </div>
 
         <div className='credit-card__number'>
-          {props.number}
+          1234 1232 2343 2134
         </div>
 
         <div className='credit-card__info'>
           <div className='credit-card__info_name'>
             <div className='credit-card__info_label'>Nombre</div>
-            <div>{props.name}</div>
+            <div>Julian </div>
           </div>
 
           <div className='credit-card__info_expiry'>
             <div className='credit-card__info_label'>Valido hasta </div>
-            <div>{props.expiry}</div>
+            <div>12/24</div>
           </div>
         </div>
 
@@ -28,7 +27,6 @@ const CreditCard = () => {
 
     </div>
   );
-  };
 };
 
 export default CreditCard;
